915f80619e38c9e76635b897c32a287b0c80adf7
KungFury1 <103309926+KungFury1@users.noreply.github.com>
2023-10-08 19:03:58 +0300

    programming

---
+++ datatypes2.js
@@ -0,0 +1,94 @@
+// Пример 1: "number" + 3 + 3
+// let a = "number" + 3 + 3
+// console.log(a);
+// Result is number33
+
+// Пример 2: null + 3
+// let a = null + 3
+// console.log(a);
+// Result is 3
+
+// Пример 3: 5 && "qwerty"
+// let a = 5 && "qwerty"
+// console.log(a);
+// Result is qwerty
+
+// Пример 4: +'40' + +'2' + "hillel";
+// let a = +'40' + +'2' + "hillel"
+// console.log(a);
+// Result is 42hillel
+
+// Пример 5: '10' - 5 === 6;
+// let a = '10' - 5 === 6
+// console.log(a);
+// Result is False
+
+// Пример 6: true + false
+// let a = true + false
+// console.log(a);
+// Result is 1
+
+// Пример 7: '4px' - 3
+// let a = '4px' - 3
+// console.log(a);
+// Result is NaN
+
+// Пример 8: '4' - 3
+// let a = '4' - 3
+// console.log(a);
+// Result is 1
+
+// Пример 9: '6' + 3 ** 0;
+// let a = '6' + 3 ** 0
+// console.log(a);
+// Result is 61
+
+// Пример 10: 12 / '6'
+// let a = 12 / '6'
+// console.log(a);
+// Result is 2
+
+// Пример 11: '10' + (5 === 6);
+// let a = '10' + (5 === 6)
+// console.log(a);
+// Result is 10false
+
+// Пример 12: null == ''
+// let a = null == ''
+// console.log(a);
+// Result is False
+
+// Пример 13: 3 ** (9 / 3);
+// let a = 3 ** (9 / 3)
+// console.log(a);
+// Result is 27
+
+// Пример 14: !!'false' == !!'true'
+// let a = !!'false' == !!'true'
+// console.log(a);
+// Result is True
+
+// Пример 15: 0 || '0' && 1
+// let a = 0 || '0' && 1
+// console.log(a);
+// Result is 1
+
+// Пример 16: (+null == false) < 1;
+// let a = (+null == false) < 1
+// console.log(a);
+// Result is false
+
+// Пример 17: false && true || true
+// let a = false && true || true
+// console.log(a);
+// Result is true
+
+// Пример 18: false && (false || true);
+// let a = false && (false || true)
+// console.log(a);
+// Result is False
+
+// Пример 19: (+null == false) < 1 ** 5;
+// let a = (+null == false) < 1 ** 5
+// console.log(a);
+// Result is false
