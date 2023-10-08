c01e3241479ef4bb5f150dd8e2672d4d583d1fb0
KungFury1 <103309926+KungFury1@users.noreply.github.com>
2023-10-08 23:11:34 +0300

    Final

--- datatypes2.js
+++ datatypes2.js
@@ -1,94 +1,113 @@
 // Пример 1: "number" + 3 + 3
 // let a = "number" + 3 + 3
 // console.log(a);
-// Result is number33
+// console.log(typeof a);
+// Result is number33 (string)
 
 // Пример 2: null + 3
 // let a = null + 3
 // console.log(a);
-// Result is 3
+// console.log(typeof a);
+// Result is 3 (number)
 
 // Пример 3: 5 && "qwerty"
 // let a = 5 && "qwerty"
 // console.log(a);
-// Result is qwerty
+// console.log(typeof a);
+// Result is qwerty (string)
 
 // Пример 4: +'40' + +'2' + "hillel";
 // let a = +'40' + +'2' + "hillel"
 // console.log(a);
-// Result is 42hillel
+// console.log(typeof a);
+// Result is 42hillel (string)
 
 // Пример 5: '10' - 5 === 6;
 // let a = '10' - 5 === 6
 // console.log(a);
-// Result is False
+// console.log(typeof a);
+// Result is False (boolean)
 
 // Пример 6: true + false
 // let a = true + false
 // console.log(a);
-// Result is 1
+// console.log(typeof a);
+// Result is 1 (number)
 
 // Пример 7: '4px' - 3
 // let a = '4px' - 3
 // console.log(a);
-// Result is NaN
+// console.log(typeof a);
+// Result is NaN (number)
 
 // Пример 8: '4' - 3
 // let a = '4' - 3
 // console.log(a);
-// Result is 1
+// console.log(typeof a);
+// Result is 1 (number)
 
 // Пример 9: '6' + 3 ** 0;
 // let a = '6' + 3 ** 0
 // console.log(a);
-// Result is 61
+// console.log(typeof a);
+// Result is 61 (string)
 
 // Пример 10: 12 / '6'
 // let a = 12 / '6'
 // console.log(a);
-// Result is 2
+// console.log(typeof a);
+// Result is 2 (number)
 
 // Пример 11: '10' + (5 === 6);
 // let a = '10' + (5 === 6)
 // console.log(a);
-// Result is 10false
+// console.log(typeof a);
+// Result is 10false or just 10 (string)
 
 // Пример 12: null == ''
 // let a = null == ''
 // console.log(a);
-// Result is False
+// console.log(typeof a);
+// Result is False (boolean)
 
 // Пример 13: 3 ** (9 / 3);
 // let a = 3 ** (9 / 3)
 // console.log(a);
-// Result is 27
+// console.log(typeof a);
+// Result is 27 (number)
 
 // Пример 14: !!'false' == !!'true'
 // let a = !!'false' == !!'true'
 // console.log(a);
-// Result is True
+// console.log(typeof a);
+// Result is True (boolean)
 
 // Пример 15: 0 || '0' && 1
 // let a = 0 || '0' && 1
 // console.log(a);
-// Result is 1
+// console.log(typeof a);
+// Result is 1 (number)
 
 // Пример 16: (+null == false) < 1;
 // let a = (+null == false) < 1
 // console.log(a);
-// Result is false
+// console.log(typeof a);
+// Result is false (boolean)
 
 // Пример 17: false && true || true
 // let a = false && true || true
 // console.log(a);
-// Result is true
+// console.log(typeof a);
+// Result is true (boolean)
 
 // Пример 18: false && (false || true);
 // let a = false && (false || true)
 // console.log(a);
-// Result is False
+// console.log(typeof a);
+// Result is False (bollean)
 
 // Пример 19: (+null == false) < 1 ** 5;
 // let a = (+null == false) < 1 ** 5
 // console.log(a);
-// Result is false
+// console.log(typeof a);
+// Result is false (boolean)
