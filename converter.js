1e35d223543ce0b2e30c93565c1d11aca741eca4
KungFury1 <103309926+KungFury1@users.noreply.github.com>
2023-10-09 13:16:33 +0300

    completed2

--- converter.js
+++ converter.js
@@ -1,34 +1,34 @@
-const value = prompt("Enter the value (Km, Hrs, Kg):");
-const measure = +prompt("Enter measure:")
-let result;
-    switch (value) {
-        case "Km":
-            result = measure * 1000 + " meters";
-            break;
-        case "Hrs":
-            result = measure * 60 + "minutes";
-            break;
-        case "Kg":
-            result = measure * 1000 + "grams";
-            break;
-        default:
-            result = "Unknown value";
-            break;
-    }
-   alert(result);
-
-// const value = "Km";
-// const measure = 10
+// const value = prompt("Enter the value (Km, Hrs, Kg):");
+// const measure = +prompt("Enter measure:")
 // let result;
 //     switch (value) {
 //         case "Km":
-//             result = measure * 1000 + "meters";
+//             result = measure * 1000 + " meters";
 //             break;
 //         case "Hrs":
-//             result = measure * 60 + "minutes";
+//             result = measure * 60 + " minutes";
 //             break;
 //         case "Kg":
-//             result = measure * 1000 + "grams";
+//             result = measure * 1000 + " grams";
+//             break;
+//         default:
+//             result = "Unknown value";
+//             break;
+//     }
+//    alert(result);
+
+// const value = "Hrs";
+// const measure = 1
+// let result;
+//     switch (value) {
+//         case "Km":
+//             result = measure * 1000 + " meters";
+//             break;
+//         case "Hrs":
+//             result = measure * 60 + " minutes";
+//             break;
+//         case "Kg":
+//             result = measure * 1000 + " grams";
 //             break;
 //         default:
 //             result = "Unknown value";
