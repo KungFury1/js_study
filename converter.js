4a951a43da40e2d0f8bec1783e3af75bdfd26948
KungFury1 <103309926+KungFury1@users.noreply.github.com>
2023-10-09 14:48:43 +0300

    Fix to header

--- converter.js
+++ converter.js
@@ -1,21 +1,21 @@
-var value = prompt("Enter the value (Km, Hrs, Kg):");
-var measure = prompt("Enter measure:")
-var result;
-    switch (value) {
-        case "Km":
-            result = measure * 1000 + " meters";
-            break;
-        case "Hrs":
-            result = measure * 60 + " minutes";
-            break;
-        case "Kg":
-            result = measure * 1000 + " grams";
-            break;
-        default:
-            result = "Unknown value";
-            break;
-    }
-   alert(result);
+// var value = prompt("Enter the value (Km, Hrs, Kg):");
+// var measure = prompt("Enter measure:")
+// var result;
+//     switch (value) {
+//         case "Km":
+//             result = measure * 1000 + " meters";
+//             break;
+//         case "Hrs":
+//             result = measure * 60 + " minutes";
+//             break;
+//         case "Kg":
+//             result = measure * 1000 + " grams";
+//             break;
+//         default:
+//             result = "Unknown value";
+//             break;
+//     }
+//    alert(result);
 
 // var value = "Hrs";
 // var measure = 1
