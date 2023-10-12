788ccbad22158de0675713c511caa92469018ce3
KungFury1 <103309926+KungFury1@users.noreply.github.com>
2023-10-12 03:28:13 +0300

    Fix

--- age.js
+++ age.js
@@ -1,20 +1,20 @@
-var year = prompt("Вкажіть скільки вам років");
+// var year = prompt("Вкажіть скільки вам років");
 
-var result;
-if (year < 0) {
-    result = year + " від'ємно живий";
-} else if (year == 0) {
-    result = year + " років ще не народився";
-} else if (year % 10 == 0) {
-    result = year + " років";
-} else if (year % 10 == 1) {
-    result = year + " рік";
-} else if (year % 10 > 4) {
-    result = year + " років";
-} else {
-    result = year + " роки";
-}
-alert(result);
+// var result;
+// if (year < 0) {
+//     result = year + " від'ємно живий";
+// } else if (year == 0) {
+//     result = year + " років ще не народився";
+// } else if (year % 10 == 0) {
+//     result = year + " років";
+// } else if (year % 10 == 1) {
+//     result = year + " рік";
+// } else if (year % 10 > 4) {
+//     result = year + " років";
+// } else {
+//     result = year + " роки";
+// }
+// alert(result);
 
 
 
