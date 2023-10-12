c01f91dd6355ec038f80a204348af9b42ad9cfc2
KungFury1 <103309926+KungFury1@users.noreply.github.com>
2023-10-12 03:24:53 +0300

    Completed

---
+++ age.js
@@ -0,0 +1,33 @@
+var year = prompt("Вкажіть скільки вам років");
+
+var result;
+if (year < 0) {
+    result = year + " від'ємно живий";
+} else if (year == 0) {
+    result = year + " років ще не народився";
+} else if (year % 10 == 0) {
+    result = year + " років";
+} else if (year % 10 == 1) {
+    result = year + " рік";
+} else if (year % 10 > 4) {
+    result = year + " років";
+} else {
+    result = year + " роки";
+}
+alert(result);
+
+
+
+// Нижче не частина мого домашнього завдання, залишив собі на згадку бо коли я написав цей код з нього народився той що вище. 
+// if (year < 0) {
+//     alert("Від'ємно живий");
+// } else if (year == 0) {
+//     alert("Років ще не народився");
+// } else if (year % 10 == 1) {
+//     alert("Рік");
+// } else if (year % 10 > 4) {
+//     alert("Років");
+// } else {
+//     alert("Роки");
+// };
+
