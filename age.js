fc0872926fd06477bef9c65ad86198a45077d754
KungFury1 <103309926+KungFury1@users.noreply.github.com>
2023-10-12 14:22:28 +0300

    modified2

--- age.js
+++ age.js
@@ -1,22 +1,22 @@
-var year = +prompt("Вкажіть скільки вам років");
+// var year = +prompt("Вкажіть скільки вам років");
 
-var result;
-if (year < 0) {
-    result = "Від'ємно живий"; // можно було зробити просто алерт інвалід параметерс але вирішив трохи меми згадати)
-} else if (year == 0) {
-    result = "Ще не народився"; // нуль років так то але з рештою ще не народився :))
-} else if (year % 10 == 0) {
-    result = year + " років";
-} else if (year % 50 == 11) {
-    result = year + " років";
-} else if (year % 10 == 1) {
-    result = year + " рік";
-} else if (year % 10 > 4) {
-    result = year + " років";
-} else {
-    result = year + " роки";
-}
-alert(result);
+// var result;
+// if (year < 0) {
+//     result = "Від'ємно живий"; // можно було зробити просто алерт інвалід параметерс але вирішив трохи меми згадати)
+// } else if (year == 0) {
+//     result = "Ще не народився"; // нуль років так то але з рештою ще не народився :))
+// } else if (year % 10 == 0) {
+//     result = year + " років";
+// } else if (year % 50 == 11) {
+//     result = year + " років";
+// } else if (year % 10 == 1) {
+//     result = year + " рік";
+// } else if (year % 10 > 4) {
+//     result = year + " років";
+// } else {
+//     result = year + " роки";
+// }
+// alert(result);
 
 
 
