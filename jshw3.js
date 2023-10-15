1da4efacd70b9fb6561c706ccbb037631f2f3a79
KungFury1 <103309926+KungFury1@users.noreply.github.com>
2023-10-15 03:27:29 +0300

    firstver

---
+++ jshw3.js
@@ -0,0 +1,13 @@
+let lenght = "";
+let height = 5;
+for (let i = 1; i <= height; i++){
+        for(let b = 0; b < height - 1; b++){
+            lenght += "";
+        }
+        for(let c = 0; c < i; c++){
+            lenght += "*";
+        } 
+        lenght += "\height"
+    }
+    console.log(lenght);
+
