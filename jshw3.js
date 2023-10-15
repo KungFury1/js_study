0252468a8c37ff1b7a8f64ce551fda3571506f5a
KungFury1 <103309926+KungFury1@users.noreply.github.com>
2023-10-15 03:29:39 +0300

    fix2

--- jshw3.js
+++ jshw3.js
@@ -1,13 +1,50 @@
-let lenght = "";
-let height = 5;
-for (let i = 1; i <= height; i++){
-        for(let b = 0; b < height - 1; b++){
-            lenght += "";
-        }
-        for(let c = 0; c < i; c++){
-            lenght += "*";
-        } 
-        lenght += "\height"
-    }
-    console.log(lenght);
+// function triangle(height) {
+//    let lenght = "";
+//    for (let i = 1; i <= height; i++) {
+//        for (let j = 1; j <= i; j++){
+//         lenght += "*";
+//     }
+//     lenght += "\n";
+//    }
+//    return lenght;
+// }
+// console.log(triangle(5));
 
+// function triangle2(height) {
+//      let lenght = "";
+//      let symbol = "*";
+//      let line = "";
+//      let breaker = "\n";
+//      for (let i = 0; i < height; i++){
+//         line += symbol;
+//         lenght += line + breaker;
+//      }
+//     return lenght;
+// }
+// console.log(triangle2(5))
+
+
+// let a = 0
+// let b = 0
+// let dif = 0
+// for (let c = 1; c <= 100; c++) {
+//     a = a + c;
+// } for (let x = 1; x <= 100; x++) {
+//     if (x % 3) continue; 
+// { b = b + x;
+//     }
+// } dif = a - b;
+// console.log(dif)
+
+// function math(x, y) {
+//     if (x > 0 && y == 0) {
+//         return undefined;
+//     }
+//     a = x;
+//     while(y > 1) {
+//     a = x * a;
+//     y = --y;
+//     }
+//     return a;
+// } 
+// console.log(math(3, 2));
