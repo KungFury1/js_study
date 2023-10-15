fd5d06626f0713f28d69995ad109e877eb400577
KungFury1 <103309926+KungFury1@users.noreply.github.com>
2023-10-15 13:56:41 +0300

    finalever

--- jshw3.js
+++ jshw3.js
@@ -1,3 +1,4 @@
+// трикутники
 // function triangle(height) {
 //    let lenght = "";
 //    for (let i = 1; i <= height; i++) {
@@ -23,28 +24,24 @@
 // }
 // console.log(triangle2(5))
 
+// сума не кратних 3
+// let sum = 0;
 
-// let a = 0
-// let b = 0
-// let dif = 0
-// for (let c = 1; c <= 100; c++) {
-//     a = a + c;
-// } for (let x = 1; x <= 100; x++) {
-//     if (x % 3) continue; 
-// { b = b + x;
-//     }
-// } dif = a - b;
-// console.log(dif)
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 !== 0) {
+//     sum += i;
+//   }
+// }
+// console.log(sum)
 
-// function math(x, y) {
-//     if (x > 0 && y == 0) {
-//         return undefined;
-//     }
-//     a = x;
-//     while(y > 1) {
-//     a = x * a;
-//     y = --y;
-//     }
-//     return a;
-// } 
-// console.log(math(3, 2));
+// піднесення до ступеня
+// function pow(x, y) {
+//   let result = 1;
+
+//   for (let i = 0; i < y; i++) {
+//     result *= x;
+//   }
+
+//   return result;
+// }
+// console.log(pow(4, 2));
