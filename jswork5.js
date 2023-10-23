5646914f366e2e486965574bd1ba0b9ccf0ff257
KungFury1 <103309926+KungFury1@users.noreply.github.com>
2023-10-23 03:04:40 +0300

    first step

---
+++ jswork5.js
@@ -0,0 +1,37 @@
+// var codex = {
+// "\nversion": " 10th Edition.",
+// "faction": " Tyranids.",
+// "releaseDate": " 2023.",
+// }
+
+// for (let key in codex){
+//     console.log(key + ': ' + codex[key]);
+// }
+// codex.newcodex = " 11th Edition"
+// for (let key in codex){
+//     console.log(key + ': ' + codex[key]);
+//     }
+
+// var services = {
+// 	"стрижка": "60",
+// 	"гоління": "80",
+// 	"Миття голови": "100",
+// };
+
+// var keys = Object.keys(services);
+// var minPrice = +services[keys[0]]; 
+// var i;
+
+// for (i = 1; i < keys; i++) {
+//     var value = services[keys[i]];
+//     if (value <= minPrice) minPrice = value;
+// }
+
+// maxPrice = 0;
+// for (let keys in services) {
+//   maxPrice += +services[keys];
+// }
+
+// console.log(minPrice + " грн");
+// console.log(maxPrice + " грн");
+
