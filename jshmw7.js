803ad19b191a21a8c75fcc0b7cf7ba38b490b563
KungFury1 <103309926+KungFury1@users.noreply.github.com>
2023-11-01 23:51:05 +0200

    done

---
+++ jshmw7.js
@@ -0,0 +1,34 @@
+// var str = "sdfhslkhlaASDlsdlsg";
+// var regexp = /[^a|A]/g;
+// console.log("Our search: " + str);
+// console.log(str.search(regexp));
+// var res = str.match(regexp);
+// console.log(res);
+
+
+// var arr = [
+//     {
+//         userName:"Test",
+//         lastName:"Test",
+//         email:"test.test@gmail.com"
+//     },
+//     {
+//         userName:"Dmitro",
+//         lastName:"Porohov",
+//         email:"dmitro.porohov@yahoo.com"
+//     },
+//     {
+//         userName:"Andrii",
+//         lastName:"",
+//         email:"andrii@mail.ru" 
+//     },
+// ];
+
+// var validEmailRegex = /[-.\w]+@([yahoo]|g[mail]+\.)+[\w-]+/gi;
+// var validEmails = arr.filter((item) => {
+//   return item.email.match(validEmailRegex);
+// });
+
+// console.log(validEmails);
+
+
